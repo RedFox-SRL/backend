df16875a922b1a584b00085b87551f6c36348d5f
Catiel <oliver.alandiaa@gmail.com>
2024-09-14 10:45:00 -0400

    Add new API response codes for invalid email verification URL and already verified email

--- resources/lang/en/api.php
+++ resources/lang/en/api.php
@@ -4,5 +4,6 @@ return [
     'invalid_credentials' => 'Invalid email or password.',
     'something_went_wrong' => 'Something went wrong.',
     'validation_error' => 'Validation error.',
-
+    'invalid_email_verification_link' => 'Invalid/Expired url provider.',
+    'email_already_verified' => 'Email already verified.',
 ];
