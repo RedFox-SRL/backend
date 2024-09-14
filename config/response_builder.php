fb6785d12a4ffa359f3c55a7c641a75504dde8f4
Catiel <oliver.alandiaa@gmail.com>
2024-09-14 15:17:55 -0400

    Add new API response code for invalid reset password token

--- config/response_builder.php
+++ config/response_builder.php
@@ -37,5 +37,6 @@ return [
         ApiCode::VALIDATION_ERROR => 'api.validation_error',
         ApiCode::INVALID_EMAIL_VERIFICATION_URL => 'api.invalid_email_verification_link',
         ApiCode::EMAIL_ALREADY_VERIFIED => 'api.email_already_verified',
+        ApiCode::INVALID_RESET_PASSWORD_TOKEN => 'api.invalid_reset_password_token',
     ],
 ];
