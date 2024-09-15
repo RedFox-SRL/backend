ae8cae9693e2c395926d4ad4c9013104b54019a7
Catiel <oliver.alandiaa@gmail.com>
2024-09-15 17:33:17 -0400

    Add model instances for Student and Teacher based on user role when registering a new user

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -3,14 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegistrationRequest;
-use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Student;
+use App\Models\Teacher;
+use Illuminate\Support\Facades\DB;
 
 class RegistrationController extends Controller
 {
     public function register(RegistrationRequest $request)
     {
-        User::create($request->getAttributes())->sendEmailVerificationNotification();
+        DB::transaction(function () use ($request) {
+            $user = User::create($request->getAttributes());
+            $user->sendEmailVerificationNotification();
+
+            if ($user->role == 'student') {
+                Student::create(['user_id' => $user->id]);
+            } elseif ($user->role == 'teacher') {
+                Teacher::create(['user_id' => $user->id]);
+            }
+        });
 
         return $this->respondWithMessage('User registered successfully');
     }
