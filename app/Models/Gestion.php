b110428732502ae7aba293d4c380a808bb0f2905
Catiel <oliver.alandiaa@gmail.com>
2024-09-17 19:57:30 -0400

    Add modelo Gestion for managing teacher's groups and their limits

---
+++ app/Models/Gestion.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Gestion extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'teacher_id',
+        'code',
+        'semester',
+        'start_date',
+        'end_date',
+        'group_limit',
+        'is_code_active',
+    ];
+
+    protected $table = 'gestiones';
+
+    // Relación con el modelo Teacher
+    public function teacher()
+    {
+        return $this->belongsTo(Teacher::class);
+    }
+
+    // Genera un código único
+    public static function generateUniqueCode()
+    {
+        do {
+            $code = strtoupper(bin2hex(random_bytes(3))); // Genera un código aleatorio de 6 caracteres
+        } while (self::where('code', $code)->exists());
+
+        return $code;
+    }
+}
