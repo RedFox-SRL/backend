6f252e054228f504b281c1e2bc7f3027a6552f27
Catiel <oliver.alandiaa@gmail.com>
2024-09-19 14:07:24 -0400

    Refactor CreateGroupRequest and JoinGroupRequest to handle group creation, joining, and validation

---
+++ app/Http/Requests/CreateGroupRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateGroupRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'short_name' => 'required|string|max:255',
+            'long_name' => 'required|string|max:255',
+            'contact_email' => 'required|email',
+            'contact_phone' => 'required|string',
+            'logo' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ];
+    }
+}
