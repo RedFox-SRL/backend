d6da4bc7185feb1121c7263d69a19baa36917a77
Catiel <oliver.alandiaa@gmail.com>
2024-09-19 16:04:40 -0400

    Refactor Group model to update field names and data types

--- database/migrations/2024_09_18_001846_create_groups_table.php
+++ database/migrations/2024_09_18_001846_create_groups_table.php
@@ -17,18 +17,16 @@ class CreateGroupsTable extends Migration
             $table->id();
             $table->foreignId('creator_id')->constrained('students')->onDelete('set null');
             $table->foreignId('management_id')->constrained('management')->onDelete('cascade');
-            $table->string('code')->unique(); // Código único para invitar estudiantes
-            $table->string('nombre_corto');
-            $table->string('nombre_largo');
-            $table->string('correo_contacto');
-            $table->string('telefono_contacto');
-            $table->string('logo')->nullable(); // Opcional
-            $table->boolean('is_code_active')->default(true); // Controla si el código está activo
-            $table->integer('max_miembros'); // Máximo número de miembros basado en la configuración del docente
+            $table->string('code')->unique();
+            $table->string('short_name');
+            $table->string('long_name');
+            $table->string('contact_email');
+            $table->string('contact_phone');
+            $table->string('logo')->nullable();
+            $table->boolean('is_code_active')->default(true);
             $table->timestamps();
         });
     }
-
 
     /**
      * Reverse the migrations.
