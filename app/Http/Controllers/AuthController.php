f6b2bc3695320e2acb507ab2f454303fe26fb217
Catiel <oliver.alandiaa@gmail.com>
2024-09-22 23:46:00 -0400

    ttl to 30 days

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
         return $this->respond([
             'token' => $token,
             'access_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'expires_in' => auth()->factory()->getTTL() * 60 * 24 * 30
         ]);
     }
 
