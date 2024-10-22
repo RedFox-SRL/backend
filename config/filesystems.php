81d00b2ea354c3e4990b98b760a6923ada4318e9
Catiel <oliver.alandiaa@gmail.com>
2024-10-22 17:33:13 -0400

    migrate to digital ocean spaces p

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,6 +60,7 @@ return [
             'region' => env('DO_SPACES_REGION'),
             'bucket' => env('DO_SPACES_BUCKET'),
             'endpoint' => env('DO_SPACES_ENDPOINT'),
+            'visibility' => 'public',
             'url' => env('DO_URL'),
         ],
 
