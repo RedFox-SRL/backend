4cacd607042e2b578c539d1f1a384977e459cc24
Catiel <oliver.alandiaa@gmail.com>
2024-11-01 14:20:35 -0400

    update changes for sprints and task

--- routes/api.php
+++ routes/api.php
@@ -11,19 +11,10 @@ use App\Http\Controllers\StudentManagementController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\VerificationController;
+use App\Http\Controllers\WeeklyEvaluationController;
+use App\Http\Controllers\SprintEvaluationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -52,17 +43,25 @@ Route::middleware(['api'])->group(function () {
     Route::put('managements/{management}/update-group-limit', [ManagementController::class, 'updateGroupLimit']);
     Route::post('managements/join', [StudentManagementController::class, 'join']);
     Route::post('managements/leave', [StudentManagementController::class, 'leaveManagement']);
+
     Route::post('groups', [GroupController::class, 'create']);
     Route::get('groups/details', [GroupController::class, 'getGroupDetails']);
     Route::post('groups/leave', [GroupController::class, 'leaveGroup']);
     Route::get('managements/{management}/groups', [GroupController::class, 'getGroupsByManagement']);
     Route::post('groups/join', [GroupController::class, 'joinGroup']);
+    Route::post('groups/remove-member/{memberId}', [GroupController::class, 'removeMember']);
+    Route::post('groups/assign-role/{memberId}', [GroupController::class, 'assignRole']);
+    Route::get('groups/{groupId}/members', [GroupController::class, 'getGroupMembersWithRoles']);
+    Route::put('groups/contact-info', [GroupController::class, 'updateContactInfo']);
+    Route::get('/group-names', [GroupController::class, 'getGroupNames']);
 
     Route::get('/sprints', [SprintController::class, 'index']);
     Route::post('/sprints', [SprintController::class, 'store']);
     Route::get('/sprints/{id}', [SprintController::class, 'show']);
     Route::put('/sprints/{id}', [SprintController::class, 'update']);
     Route::delete('/sprints/{id}', [SprintController::class, 'destroy']);
+    Route::get('/sprints/{id}/tasks', [SprintController::class, 'getTasks']);
+    Route::get('/sprints/{id}/evaluation-summary', [SprintController::class, 'getEvaluationSummary']);
 
     Route::get('/tasks', [TaskController::class, 'index']);
     Route::post('/tasks', [TaskController::class, 'store']);
@@ -70,18 +69,14 @@ Route::middleware(['api'])->group(function () {
     Route::put('/tasks/{id}', [TaskController::class, 'update']);
     Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
 
-    Route::get('/group-names', [GroupController::class, 'getGroupNames']);
-
-    Route::get('/sprints/{id}/evaluation-template', [SprintController::class, 'getEvaluationTemplate']);
-    Route::post('/sprints/{id}/submit-evaluation', [SprintController::class, 'submitEvaluation']);
-    Route::get('/sprints/{id}/evaluated-tasks', [SprintController::class, 'getEvaluatedTasks']);
-
-    Route::post('groups/remove-member/{memberId}', [GroupController::class, 'removeMember']);
-    Route::post('groups/assign-role/{memberId}', [GroupController::class, 'assignRole']);
-    Route::get('groups/{groupId}/members', [GroupController::class, 'getGroupMembersWithRoles']);
-
-    Route::put('groups/contact-info', [GroupController::class, 'updateContactInfo']);
-
     Route::post('/announcements', [AnnouncementController::class, 'store']);
     Route::get('/management/{managementId}/announcements', [AnnouncementController::class, 'index']);
+
+    Route::get('/sprints/{id}/weekly-evaluation-template', [WeeklyEvaluationController::class, 'getEvaluationTemplate']);
+    Route::post('/sprints/{id}/weekly-evaluation', [WeeklyEvaluationController::class, 'create']);
+    Route::get('/sprints/{id}/weekly-evaluations', [WeeklyEvaluationController::class, 'getWeeklyEvaluations']);
+
+    Route::get('/sprints/{id}/sprint-evaluation-template', [SprintEvaluationController::class, 'getEvaluationTemplate']);
+    Route::post('/sprints/{id}/sprint-evaluation', [SprintEvaluationController::class, 'create']);
+    Route::get('/sprints/{id}/sprint-evaluation', [SprintEvaluationController::class, 'getFinalEvaluation']);
 });
