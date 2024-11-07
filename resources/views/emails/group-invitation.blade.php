f4ef31543efbfc262cac1a20598a5bb50d50a558
Catiel <oliver.alandiaa@gmail.com>
2024-11-07 17:43:28 -0400

    add invitations

--- resources/views/emails/group-invitation.blade.php
+++ resources/views/emails/group-invitation.blade.php
@@ -10,9 +10,9 @@
 <p>Hola {{ $invitation->invitedStudent->user->name }},</p>
 <p>Has sido invitado a unirte al grupo "{{ $invitation->group->short_name }}" en el sistema de gestión de proyectos.</p>
 <p>Para aceptar la invitación, haz clic en el siguiente enlace:</p>
-<a href="{{ url('/invitations/accept/' . $invitation->token) }}">Aceptar invitación</a>
+<a href="{{ url('/api/invitations/accept/' . $invitation->token) }}">Aceptar invitación</a>
 <p>Si no deseas unirte al grupo, puedes rechazar la invitación haciendo clic aquí:</p>
-<a href="{{ url('/invitations/reject/' . $invitation->token) }}">Rechazar invitación</a>
+<a href="{{ url('/api/invitations/reject/' . $invitation->token) }}">Rechazar invitación</a>
 <p>Esta invitación expirará en 6 horas.</p>
 <p>Si no has solicitado esta invitación, puedes ignorar este correo electrónico.</p>
 </body>
