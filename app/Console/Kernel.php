2169f87aec4e3f24f80ced3dec36a397695df89f
Catiel <oliver.alandiaa@gmail.com>
2024-11-15 18:15:52 -0400

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,13 +19,13 @@ class Kernel extends ConsoleKernel
             foreach ($sprints as $sprint) {
                 Http::post("/api/sprints/{$sprint->id}/finish");
             }
-        })->daily();
+        })->everyMinute();
 
         // Enviar recordatorios de evaluaciones
         $schedule->call(function () {
             $evaluationService = app(EvaluationService::class);
             $evaluationService->sendReminders();
-        })->daily();
+        })->everyMinute();
     }
 
     protected function commands()
